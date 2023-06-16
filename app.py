3b5a880a348e6429d4a65b2720f10cd280758c42
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-16 17:21:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -105,11 +105,33 @@ def main():
     if int(Age)-int(Tenure)<18: 
         st.error('Внимание проверьте данные возраст и срок обслуживания')
         
-       
+    if CreditScore < 100 and Balance < 2000 and EstimatedSalary < 2000 and IsActiveMember == 0 and Age > 80 and Tenure == 0:
+            st.success('Вероятность оттока составляет более 70%.')
+            st.markdown(churn_html, unsafe_allow_html= True)
+
+    if CreditScore < 200 and Balance < 10000 and EstimatedSalary < 10000 and IsActiveMember == 0 and Age > 70 and Tenure == 1:
+            st.success('Вероятность оттока составляет более 60%.')
+            st.markdown(churn_html, unsafe_allow_html= True)
+
+    if CreditScore < 300 and Balance < 15000 and EstimatedSalary < 15000 and IsActiveMember == 0 and Age > 60 and Tenure == 2:
+            st.success('Вероятность оттока составляет более 50%.')
+            st.markdown(churn_html, unsafe_allow_html= True)
+
+    if CreditScore > 100 and EstimatedSalary > 5000 and IsActiveMember == 1 and NumOfProducts > 1 and Age < 60 and Tenure > 3 and Balance > 5000:
+            st.success('Вероятность оттока составляет менее 30%.')
+            st.markdown(churn_html, unsafe_allow_html= True)
+  
+    if CreditScore > 200 and EstimatedSalary > 10000 and IsActiveMember == 1 and NumOfProducts > 2 and Age < 50 and Tenure > 4 and Balance > 10000:
+            st.success('Вероятность оттока составляет менее 20%.')
+            st.markdown(churn_html, unsafe_allow_html= True)
+
+    if CreditScore > 300 and EstimatedSalary > 15000 and IsActiveMember == 1 and NumOfProducts > 3 and Age < 40 and Tenure > 5 and Balance > 20000:
+            st.success('Вероятность оттока составляет менее 10%.')
+            st.markdown(churn_html, unsafe_allow_html= True)
     
     if st.button('Сделать прогноз'):
         output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
-        if Age<22 and Balance<2000.00 and NumOfProducts==1 and HasCrCard==0 and IsActiveMember==0:
+        #if Age<22 and Balance<2000.00 and NumOfProducts==1 and HasCrCard==0 and IsActiveMember==0:
             output<0.5
             
             
