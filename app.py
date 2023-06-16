3d192b3eaff1d482725859c7adc813793a64fac4
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-16 17:34:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -138,11 +138,11 @@ def main():
         st.success('Вероятность оттока составляет {}'.format(output))
 
 
-        if output >= 0.5:
-            st.markdown(churn_html, unsafe_allow_html= True)
+        # if output >= 0.5:
+        #     st.markdown(churn_html, unsafe_allow_html= True)
 
-        else:
-            st.markdown(no_churn_html, unsafe_allow_html= True)
+        # else:
+        #     st.markdown(no_churn_html, unsafe_allow_html= True)
 
 if __name__=='__main__':
     main()
