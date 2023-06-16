64184be7e423962203042a5d2beb377ecd26414c
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-16 16:06:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,20 +23,39 @@ st.write("""
 Эта модель предсказывает уйдет клиент банка или останется!
 """)
 
-def user_input_features():
-    Age = st.slider('Возраст', 18, 92, 31)
-    Balance = st.slider('Баланс', 0.00, 12544.90, 2000.00)
-    NumOfProducts = st.slider('Количество продуктов', 1, 4, 2)
-    EstimatedSalary = st.slider('Зарплата', 0.00, 4444.28, 1500.00)
-    CreditScore = st.slider('Скоринговый балл', 0, 850, 400)
-    data = {'Возраст': Age,
-            'Баланс': Balance,
-            'Количество продуктов': NumOfProducts,
-            'Зарплата': EstimatedSalary,
-            'Скоринговый балл': CreditScore,}
-    features = pd.DataFrame(data, index=[0])
-    return features
-input = user_input_features()
+
+
+def main():
+    st.title("Прогноз оттока клиентов")
+    html_temp = """
+    <div style="background-color:white ;padding:10px">
+    <h2 style="color:red;text-align:center;">Заполни форму</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+CreditScore = st.slider('Скоринговый балл', 300, 900)
+
+    Geography = st.selectbox('География/регион', ['France', 'Germany', 'Spain'])
+    Geo = int(le1_pik.transform([Geography]))
+
+    Gender = st.selectbox('Пол', ['Male', 'Female'])
+    Gen = int(le_pik.transform([Gender]))
+
+    Age = st.slider("Возраст", 10, 95)
+
+    Tenure = st.selectbox("Стаж", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10', '11', '12', '13', '14', '15'])
+
+    Balance = st.slider("Баланс", 0.00, 250000.00)
+
+    NumOfProducts = st.selectbox('Количество продуктов', ['1', '2', '3', '4'])
+
+    HasCrCard = st.selectbox("Есть кредитная БПК ?", ['0', '1'])
+
+    IsActiveMember = st.selectbox("Активный клиент ?", ['0', '1'])
+
+    EstimatedSalary = st.slider("Зарплата", 0.00, 200000.00)
+
 
 def predict_churn(CreditScore, Age, Balance, NumOfProducts, EstimatedSalary):
     input = np.array([[CreditScore, Age, Balance, NumOfProducts, EstimatedSalary]]).astype(np.float64)
@@ -49,12 +68,30 @@ def predict_churn(CreditScore, Age, Balance, NumOfProducts, EstimatedSalary):
 
     return float(pred)        
 
-if st.button("Предсказать отток клиентов"):
-    if input:
-        prediction = pred(model, input)
-        st.write(prediction)
-        
-      
+   churn_html = """  
+              <div style="background-color:#f44336;padding:20px >
+               <h2 style="color:red;text-align:center;"> Жаль, но теряем клиента.</h2>
+               </div>
+            """
+    no_churn_html = """  
+              <div style="background-color:#94be8d;padding:20px >
+               <h2 style="color:green ;text-align:center;"> Ура, клиент остаётся в банке !!!</h2>
+               </div>
+            """
+
+    if st.button('Сделать прогноз'):
+        output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
+        st.success('Вероятность оттока составляет {}'.format(output))
+        st.balloons()
+
+        if output >= 0.5:
+            st.markdown(churn_html, unsafe_allow_html= True)
+
+        else:
+            st.markdown(no_churn_html, unsafe_allow_html= True)
+
+if __name__=='__main__':
+    main()
 
 st.sidebar.info('Курс Diving into Darkness of Data Science.')
 st.sidebar.info('Подготовила проект Кругленя А.М.')
