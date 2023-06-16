713d9bbab5ae69b9559240106f1b7766ce30169b
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-16 15:43:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ st.write("""
 def user_input_features():
     Age = st.slider('Возраст', 18, 92, 31)
     Balance = st.slider('Баланс', 0.00, 12544.90, 2000.00)
-    NumOfProducts = st.slider('Количество продуктов оформленных в банке', 1, 4, 2)
-    EstimatedSalary = st.slider('Размер зароботной платы', 0.00, 4444.28, 1500.00)
-    CreditScore = st.slider('Кредитный рейтинг', 350, 850, 400)
+    NumOfProducts = st.slider('Количество продуктов', 1, 4, 2)
+    EstimatedSalary = st.slider('Зарплата', 0.00, 4444.28, 1500.00)
+    CreditScore = st.slider('Скоринговый балл', 0, 850, 400)
     data = {'Возраст': Age,
             'Баланс': Balance,
-            'Количество продуктов оформленных в банке': NumOfProducts,
-            'Размер зароботной платы': EstimatedSalary,
-            'Кредитный рейтинг': CreditScore,}
+            'Количество продуктов': NumOfProducts,
+            'Зарплата': EstimatedSalary,
+            'Скоринговый балл': CreditScore,}
     features = pd.DataFrame(data, index=[0])
     return features
 input = user_input_features()
@@ -46,4 +46,4 @@ if st.button("Предсказать отток клиентов"):
       
 
 st.sidebar.info('Курс Diving into Darkness of Data Science.')
-st.sidebar.info('ИТ АКАДЕМИЯ ПРИОРБАНК 2023. Подготовила проект Кругленя А.М.')
+st.sidebar.info('ИТ-АКАДЕМИЯ ПРИОРБАНК. 2023 год. Подготовила проект Кругленя А.М.')
