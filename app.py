86e1c2cde156efa84db0805a1daf98af92dc22eb
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 20:51:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,16 +45,9 @@ def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, Ha
     if option == 'XGBoost':
         prediction = model.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
-
-# def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
-#     input = np.array([[Balance, EstimatedSalary]]).astype(np.float64)
-#     if option == 'XGBoost':
-#         prediction = model.predict(input)
-#         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
-
-    # else:
-    #     pred=0.30
-    #     #st.markdown('Клиент может уйти, рекомендуется провести СРМ компанию')
+    else:
+        pred=0.70
+        #st.markdown('Клиент может уйти, рекомендуется провести СРМ компанию')
 
     return float(pred)
 
