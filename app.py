ca2af194119dac416ee18c682ba15d72aa617be3
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 18:07:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,10 +34,8 @@ classifier_name=['Random Forest']
 option = st.sidebar.selectbox('Алгоритм', classifier_name)
 st.subheader(option)
 
-
-
 #Importing model and label encoders
-# model=pickle.load(open("model.pkl","rb"))
+model=pickle.load(open("model.pkl","rb"))
 model = pickle.load(open("final_xg_model.pkl","rb"))
 le_pik=pickle.load(open("label_encoding_for_gender.pkl","rb"))
 le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
