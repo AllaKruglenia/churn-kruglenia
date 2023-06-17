6e72ac51254a209709dce317744c4cb4613a7560
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 22:46:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, Ha
         pred='{0:.{1}f}'.format(prediction[0][0], 2)
 
     else:
-        pred=0.50
+        pred=0.30
         st.markdown('Клиент может уйти, рекомендуется провести СРМ компанию')
     
     return float(pred)
