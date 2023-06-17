caff15646a30de61d806d6587acc2d4d6412c0fc
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 21:55:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -104,6 +104,31 @@ def main():
         else:
             st.markdown(no_churn_html, unsafe_allow_html= True)
 
+        if int(Age)-int(Tenure)<18: 
+            st.error('Внимание проверьте данные возраст и срок обслуживания')
+            
+        
+        elif Balance < 1000 and EstimatedSalary < 500 and IsActiveMember == 0 and NumOfProducts == 1:
+            st.success('Вероятность оттока составляет более 90%.')
+            st.markdown(churn_html, unsafe_allow_html= True)
+
+        elif Balance > 2000 and EstimatedSalary > 2000 and CreditScore > 250:
+            # and predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)  > 40:
+            st.success('Вероятность оттока составляет менее 40 %.')
+            st.markdown(no_churn_html, unsafe_allow_html= True)
+                
+        else:
+            output = predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
+            st.success('Вероятность оттока составляет {0:.2f}'.format(output))
+            if output >= 85:
+                st.markdown(churn_html, unsafe_allow_html= True)
+            elif output >= 40:
+                st.markdown(mb_churn_html, unsafe_allow_html= True)
+            else:
+                st.markdown(no_churn_html, unsafe_allow_html= True)
+
+
+
 if __name__=='__main__':
     main()
 st.sidebar.title('ИТ-АКАДЕМИЯ ПРИОРБАНК')
