9317d88663303766d853711a3c8bdf5dfa4c1412
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 22:00:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def main():
     Gender = st.selectbox('Пол', ['Male', 'Female'])
     Gen = int(le_pik.transform([Gender]))
 
-    Age = st.slider("Возраст", 10, 95)
+    Age = st.slider("Возраст", 18, 95)
 
     Tenure = st.selectbox("Стаж", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10', '11', '12', '13', '14', '15'])
 
