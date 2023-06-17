536df6b88e51a40c516be8e2111a5ba59ab1b957
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 21:27:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -46,12 +46,12 @@ le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
     input = np.array([[Balance,  EstimatedSalary]]).astype(np.float64)
     if option == 'RandomForest':
-        prediction = mode_1.predict_proba(input)
+        prediction = model_1.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
     else:
-        pred=0.30
-        #st.markdown('Клиент может уйти, рекомендуется провести СРМ компанию')
+        prediction = model_2.predict_proba(input)
+        pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
     return float(pred)
 
