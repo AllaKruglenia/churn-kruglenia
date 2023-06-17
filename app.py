b403b3269b9cc8c41c416294b9d71836136fbbc2
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 18:35:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def main():
  
     
     if st.button('Сделать прогноз'):
-        # output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
+        output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
         #if Age<22 and Balance<2000.00 and NumOfProducts==1 and HasCrCard==0 and IsActiveMember==0:
         #    output<0.5
             
