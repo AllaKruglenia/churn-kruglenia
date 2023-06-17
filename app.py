8c44408091a0eb7176a1f05667858c04cdfb2b97
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 21:06:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,17 @@ def set_png_as_page_bg(png_file):
     bin_str = get_base64_of_bin_file(png_file)
     page_bg_img = '''
     <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
+    .stApp {
+     background-image: url("data:image/png;base64,%s");
+     background-size: cover;
+     }
+     </style>
     ''' % bin_str
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
-set_png_as_page_bg('ottok_klientov.jpg.JPG')
+set_png_as_page_bg('ottok_klientov.jpg')
+
 
 
 classifier_name=['XGBoost', 'Гадание на кофейной гуще']
