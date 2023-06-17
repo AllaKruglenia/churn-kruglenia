af60dbbf6ef716e4b873d8ecbc99106239778202
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-17 17:39:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.subheader(option)
 
 #Importing model and label encoders
 # model=pickle.load(open("model.pkl","rb"))
-model_1 = pickle.load(open("final_xg_model.pkl","rb"))
+model = pickle.load(open("final_xg_model.pkl","rb"))
 le_pik=pickle.load(open("label_encoding_for_gender.pkl","rb"))
 le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 
@@ -46,7 +46,7 @@ le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
     input = np.array([[Balance, EstimatedSalary]]).astype(np.float64)
     if option == 'Random Forest':
-        prediction = le_pik=pickle.predict_proba(input)
+        prediction = model.predict_proba(input)
         pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
     return float(pred)
