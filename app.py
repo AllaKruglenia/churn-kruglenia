1f3d896c641e726d6dc89036223fb03716d95277
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-18 10:35:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,35 +53,26 @@ def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, Ha
 
 
 def main():
-    st.title("Прогноз оттока клиентов")
+    # st.title("Прогноз оттока клиентов")
     html_temp = """
     <div style="background-color:white ;padding:10px">
-    <h2 style="color:red;text-align:center;">Заполни форму</h2>
+    <h2 style="color:red;text-align:center;">Прогноз оттока клиентов</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-
     CreditScore = st.slider('Скоринговый балл', 300, 900)
-
-    Geography = st.selectbox('География/регион', ['France', 'Germany', 'Spain'])
+    Geography = st.selectbox('Страна', ['France', 'Germany', 'Spain'])
     Geo = int(le1_pik.transform([Geography]))
-
     Gender = st.selectbox('Пол', ['Male', 'Female'])
     Gen = int(le_pik.transform([Gender]))
-
     Age = st.slider("Возраст", 18, 95)
-
-    Tenure = st.selectbox("Стаж", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10', '11', '12', '13', '14', '15'])
-
+    Tenure = st.selectbox("Срок обслуживания", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10', '11', '12', '13', '14', '15'])
     Balance = st.slider("Баланс", 0.00, 250000.00)
-
     NumOfProducts = st.selectbox('Количество продуктов', ['1', '2', '3', '4'])
-
-    HasCrCard = st.selectbox("Есть кредитная БПК ?", ['0', '1'])
-
-    IsActiveMember = st.selectbox("Активный клиент ?", ['0', '1'])
-
+    HasCrCard = st.selectbox("Наличие кредитной БПК", ['0', '1'])
+    IsActiveMember = st.selectbox("Является ли активным клиентом?", ['0', '1'])
     EstimatedSalary = st.slider("Зарплата", 0.00, 200000.00)
+
 
     churn_html = """  
               <div style="background-color:#f44336;padding:20px >
@@ -93,20 +84,23 @@ def main():
                <h2 style="color:green ;text-align:center;"> Хорошая новость, клиент остаётся в банке! </h2>
                </div>
             """
+    if int(Age)-int(Tenure)<18: 
+        st.error('Внимание проверьте данные возраст и срок обслуживания')
+ 
 
     if st.button('Сделать прогноз'):
         output = predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
         st.success('Вероятность оттока составляет {}'.format(output))
 
 
-        # if output >= 0.5:
-        #     st.markdown(churn_html, unsafe_allow_html= True)
+        if output>str(0.5):
+            st.markdown(churn_html, unsafe_allow_html= True)
 
-        # else:
-        #     st.markdown(no_churn_html, unsafe_allow_html= True)
+        else:
+            st.markdown(no_churn_html, unsafe_allow_html= True)
 
-        if int(Age)-int(Tenure)<18: 
-            st.error('Внимание проверьте данные возраст и срок обслуживания')
+        # if int(Age)-int(Tenure)<18: 
+        #     st.error('Внимание проверьте данные возраст и срок обслуживания')
             
         
         if Balance < 1000 and EstimatedSalary < 500 and IsActiveMember == 0 and NumOfProducts == 1:
