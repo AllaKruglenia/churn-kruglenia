db6c3e58c9c826e98e2b78a1f33554b934c011f1
AllaKruglenia <132658871+AllaKruglenia@users.noreply.github.com>
2023-06-18 18:01:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -155,11 +155,12 @@ def main():
 
 # if __name__=='__main__':
 #     main()
-st.sidebar.title('ИТ-АКАДЕМИЯ ПРИОРБАНК')
-st.sidebar.title('Проект "Отток клиентов"')
+
+    st.sidebar.title('ИТ-АКАДЕМИЯ ПРИОРБАНК')
+    st.sidebar.title('Проект "Отток клиентов"')
 
 
-st.sidebar.info('Курс Diving into Darkness of Data Science.')
-st.sidebar.info('Подготовила проект Кругленя А.М.') 
+    st.sidebar.info('Курс Diving into Darkness of Data Science.')
+    st.sidebar.info('Подготовила проект Кругленя А.М.') 
 
 
